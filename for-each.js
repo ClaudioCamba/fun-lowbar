e7a71c100b52de56ed339624ddd9d2a9a3ce674b
ClaudioCamba <claudiocamba@Claudios-iMac.local>
2023-12-07 22:27:14 +0000

    update

--- for-each.js
+++ for-each.js
@@ -1,6 +1,5 @@
 function ourForEach(arrObj, func) {
     const checkArrObj = Array.isArray(arrObj);
-
     if (func){
         if (checkArrObj){
             for (let i = 0; i < arrObj.length; i++) {
